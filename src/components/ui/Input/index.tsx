ec0fda647b515e49cf29050a0b16c236a7235c5d
ianabreu <ianabreudev@gmail.com>
2023-06-18 06:15:49 -0300

    estilizando tela de login

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -1,5 +1,12 @@
+import { InputHTMLAttributes, TextareaHTMLAttributes } from "react";
 import styles from "./styles.module.scss";
 
-export function Input() {
-  return <input type="text" className={styles.input} />;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+export function Input({ ...rest }: InputProps) {
+  return <input className={styles.input} {...rest} />;
+}
+export function TextArea({ ...rest }: TextAreaProps) {
+  return <textarea className={styles.input} {...rest}></textarea>;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,21 +3,30 @@ import Image from "next/image";
 import logoSVG from "../../public/logo.svg";
 import styles from "../styles/home.module.scss";
 import { Input } from "../components/ui/Input";
+import { Button } from "../components/ui/Button";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Pede Fácil - Comanda Digital</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
       </Head>
 
       <div className={styles.containerCenter}>
-        <Image src={logoSVG} alt="Logo Pede Fácil" />
-
-        <div>
+        <div className={styles.login}>
+          <Image className={styles.logo} src={logoSVG} alt="Logo Pede Fácil" />
           <form>
-            <Input />
+            <Input placeholder="Digite seu email" type="text" />
+            <Input placeholder="Digite sua senha" type="password" />
+            <Button type="submit" loading={false}>
+              Acessar
+            </Button>
           </form>
+          <a className={styles.text}>Registrar minha empresa</a>
         </div>
       </div>
     </>
