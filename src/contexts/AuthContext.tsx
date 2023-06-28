776639021767cd7a0d9d6c13c83823871ddaeebd
ianabreu <ianabreudev@gmail.com>
2023-06-28 06:11:17 -0300

    Permanecendo dados do user

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
 import { destroyCookie, setCookie, parseCookies } from "nookies";
 import Router from "next/router";
 import { api } from "../services/apiClient";
@@ -42,6 +42,25 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps>();
   const isAutenticated = !!user;
+  useEffect(() => {
+    const { "@pedefacil.token": token } = parseCookies();
+    if (token) {
+      api
+        .get("/me")
+        .then((response) => {
+          const { id, name, email } = response.data;
+          setUser({
+            id,
+            name,
+            email,
+          });
+          console.log(id, name, email);
+        })
+        .catch(() => {
+          signOut();
+        });
+    }
+  }, []);
 
   async function signIn({ email, password }: SignInProps) {
     try {
